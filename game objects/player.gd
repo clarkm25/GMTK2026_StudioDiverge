178a86cb8b4d68f6129a8c16ff05e66a6b162611
Max Clark <97877032+clarkm25@users.noreply.github.com>
2026-07-23 14:21:39 -0700

    Boilerplate player

---
+++ game objects/player.gd
@@ -0,0 +1,32 @@
+extends CharacterBody2D
+
+@export var move_speed = 300.0
+
+func _ready():
+	pass
+	#$AnimationPlayer.play("walk_down")
+	#$AnimationPlayer.seek(0.9, true) # the standing pose of current animation
+
+func _physics_process(delta):
+	var direction = Input.get_vector("move_left","move_right","move_up", "move_down")
+	if direction:
+		velocity = direction * move_speed
+	else:
+		velocity.x = move_toward(velocity.x,0,move_speed)
+		velocity.y = move_toward(velocity.y,0,move_speed)
+		
+	move_and_slide()
+	
+	"""
+	if direction.x == 0 and direction.y == 0:
+		$AnimationPlayer.seek(0.9, true) # the standing pose of current animation
+	
+	if direction.x > 0:
+		$AnimationPlayer.play("walk_right")
+	elif direction.x < 0:
+		$AnimationPlayer.play("walk_left")
+	elif direction.y < 0:
+		$AnimationPlayer.play("walk_up")
+	elif direction.y > 0:
+		$AnimationPlayer.play("walk_down")
+	"""
