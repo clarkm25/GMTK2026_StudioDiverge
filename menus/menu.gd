37b8a191e3b149816c402c7b8b0d647a6050ad75
penny <sillimanpenny8@gmail.com>
2026-07-23 23:29:07 -0700

    menus
    Menu screen
    Options menu
    Menu music
    Game music

--- menus/menu.gd
+++ menus/menu.gd
@@ -1,7 +1,12 @@
-extends Node2D
+extends Control
 
 
 var button_type = null
+
+@onready var options = $Options
+@onready var fade_trans = $FadeTransition
+@onready var fade_timer = $FadeTransition/FadeTimer
+@onready var fade_anim = $FadeTransition/AnimationPlayer
 
 
 func _on_start_pressed() -> void:
@@ -9,22 +14,19 @@ func _on_start_pressed() -> void:
 	_fade_out()
 
 func _on_options_pressed() -> void:
-	button_type = 'options'
-	_fade_out()
+	options.pause()
 
 func _on_quit_pressed() -> void:
 	button_type = 'quit'
 	_fade_out()
 
 func _fade_out() -> void:
-	%FadeTransition.show()
-	%FadeTransition/FadeTimer.start()
-	%FadeTransition/AnimationPlayer.play('fade_out')
+	fade_trans.show()
+	fade_timer.start()
+	fade_anim.play('fade_out')
 
 func _on_fade_timer_timeout() -> void:
 	if button_type == 'start':
 		get_tree().change_scene_to_file("res://game/Main.tscn")
-	elif button_type == 'options':
-		get_tree().change_scene_to_file("")
 	elif button_type == 'quit':
 		get_tree().quit()
