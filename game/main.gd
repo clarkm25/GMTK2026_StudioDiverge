5960d03732d8efd60baf23e60dcecf9bcd35969d
penny <sillimanpenny8@gmail.com>
2026-07-25 14:35:03 -0700

    menu themes
    themes for menus

--- game/main.gd
+++ game/main.gd
@@ -1,14 +1,14 @@
 extends Node2D
 
-var fade_timer = 1.5
 @export var fade_trans : FadeTransition
-@onready var options = $CanvasLayer/Control/Options
+@export var options : Panel
 
 func _ready() -> void:
 	Game.music_player = %Music
 	Game.cleanup_fade()
 	await Game.fade_in()
 
+# TODO: convert timer to distance (half way to earth)
 var timer = 7.0
 func _process(_delta: float) -> void:
 	if timer == null: return
