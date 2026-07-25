f6d9a97635df145f4fdcbf5e025612e0bd5cfe33
penny <sillimanpenny8@gmail.com>
2026-07-25 00:17:33 -0700

    menus link
    menus link to main menu

--- menus/earth_blowup.gd
+++ menus/earth_blowup.gd
@@ -8,3 +8,8 @@ func _ready():
 	await Game.fade_in()
 	var tween = create_tween()
 	tween.tween_property(honey_label, "modulate:a", 2.5, num_sec_fade)
+
+func _on_animation_finished() -> void:
+	Game.cleanup_fade()
+	await Game.fade_out()
+	get_tree().change_scene_to_file("res://menus/Menu.tscn")
