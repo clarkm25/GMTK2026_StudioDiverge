5a95047f68ee43eed2c3237b67d8a6df69f0ab8d
n8hopp <hopper26@up.edu>
2026-07-24 17:54:53 -0700

    Fix to fade out/in for game end

--- assets/earth_blowup/earth_blowup.gd
+++ assets/earth_blowup/earth_blowup.gd
@@ -4,6 +4,7 @@ extends AnimatedSprite2D
 
 @export var num_sec_fade := 1.5
 func _ready():
+	Game.cleanup_fade()
 	await Game.fade_in()
 	var tween = create_tween()
 	tween.tween_property(honey_label, "modulate:a", 2.5, num_sec_fade)
