024ada2e351bc211d5ed7b8d02994493f4a3a354
penny <sillimanpenny8@gmail.com>
2026-07-25 17:14:10 -0700

    paralax vert
    paralax more like parastressed and paraconquered

--- game/paralax.gd
+++ game/paralax.gd
@@ -4,15 +4,14 @@ extends Sprite2D
 @export var speed_offset = 0.2
 @onready var point_of_reference = $"../../Player" # TODO: swap player for spaceship
 
-var pos_offset : Vector2
-const size = Vector2i(3000, 500)
+const size = Vector2(500, 3000)
 
-func _ready():
-	pos_offset = self.position
-
-func _process(_delta: float) -> void:
-	self.position = (-point_of_reference.position*layer*speed_offset) + pos_offset
-	if self.position.x - point_of_reference.position.x > size.x:
-		pos_offset.x -= (size.x * 2)
-	elif self.position.x - point_of_reference.position.x < -size.x:
-		pos_offset.x += (size.x * 2)
+func _physics_process(_delta: float) -> void:
+	
+	var dist = Game.home_distance
+	var speed_variance = layer*speed_offset
+	var dist_delta = clamp(Game.ship_speed * _delta, -1, 1000000000000)
+	self.position.y += speed_variance * dist_delta
+	
+	if self.position.y > size.y:
+		self.position.y -= size.y * 2
